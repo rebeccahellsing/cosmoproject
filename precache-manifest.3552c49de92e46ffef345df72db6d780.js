6346054c203c7502025d0fe9c4273553c8a7d952
Lars Mikael Gustavsson <lmtgustavsson@gmail.com>
2019-12-09 11:52:38 +0100

    Updates

---
+++ precache-manifest.3552c49de92e46ffef345df72db6d780.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "877882aae35325d728d75786ef7cad04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38cca03ad30ff855a7cd",
+    "url": "/static/css/2.1bd11376.chunk.css"
+  },
+  {
+    "revision": "b3853af0b07cb2cd5dc9",
+    "url": "/static/css/main.4976c8a5.chunk.css"
+  },
+  {
+    "revision": "38cca03ad30ff855a7cd",
+    "url": "/static/js/2.74227502.chunk.js"
+  },
+  {
+    "revision": "b3853af0b07cb2cd5dc9",
+    "url": "/static/js/main.7e299487.chunk.js"
+  },
+  {
+    "revision": "bdd2cc34068885dc47cf",
+    "url": "/static/js/runtime-main.0d290bc2.js"
+  },
+  {
+    "revision": "d499d8abed29d2ced12170635d1fd620",
+    "url": "/static/media/headerphoto2.d499d8ab.png"
+  },
+  {
+    "revision": "5880919ad0fbf6b44aaa1dc46a608668",
+    "url": "/static/media/iceberg33.5880919a.png"
+  },
+  {
+    "revision": "5fc782bec0cc333bfb7c482f6a441e6b",
+    "url": "/static/media/icebergN1.5fc782be.png"
+  },
+  {
+    "revision": "35234a8847af8227f8ce4726305410dd",
+    "url": "/static/media/icebergN222.35234a88.png"
+  },
+  {
+    "revision": "4d0304e290ca723239858c554d46a70c",
+    "url": "/static/media/instagram-logo.4d0304e2.png"
+  },
+  {
+    "revision": "ae3b5e8baeabd644ff1326f8a2ad8354",
+    "url": "/static/media/logotype.ae3b5e8b.png"
+  },
+  {
+    "revision": "2677e4ece517c5302ca2133489a2b083",
+    "url": "/static/media/micropeel.2677e4ec.jpg"
+  },
+  {
+    "revision": "351132f8661f2df97d7e04e7f76e4b12",
+    "url": "/static/media/num1.351132f8.jpg"
+  },
+  {
+    "revision": "f013e3a344e3751524eb8f089cd5df1f",
+    "url": "/static/media/num2.f013e3a3.jpg"
+  },
+  {
+    "revision": "2accfe5d8b2277b7b0540beabbd1e875",
+    "url": "/static/media/num3.2accfe5d.gif"
+  },
+  {
+    "revision": "0b952dcdad8bbd8c88a65a2173b56423",
+    "url": "/static/media/pinterest.0b952dcd.png"
+  },
+  {
+    "revision": "bc3c9b87311ca3461606ff376ed64dc3",
+    "url": "/static/media/tallpic4.bc3c9b87.gif"
+  },
+  {
+    "revision": "e697fcb8a991b438d52daa17191753bb",
+    "url": "/static/media/twitter.e697fcb8.png"
+  }
+]);
