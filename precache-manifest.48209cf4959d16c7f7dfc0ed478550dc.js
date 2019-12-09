ca76014e1cc3530794a5e3ec8a59e67ee0dfd339
Rebecca Hellsing <rebeccahellsing@Rebeccas-MBP-3.lan>
2019-12-09 20:07:16 +0100

    Updates

---
+++ precache-manifest.48209cf4959d16c7f7dfc0ed478550dc.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e6c271b9c47d1179dfffef8f5c6db2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f451eb8746ea39317293",
+    "url": "/static/css/2.1bd11376.chunk.css"
+  },
+  {
+    "revision": "2fd5fa5eac3b942ed253",
+    "url": "/static/css/main.9f73e600.chunk.css"
+  },
+  {
+    "revision": "f451eb8746ea39317293",
+    "url": "/static/js/2.c1582500.chunk.js"
+  },
+  {
+    "revision": "2fd5fa5eac3b942ed253",
+    "url": "/static/js/main.659096e8.chunk.js"
+  },
+  {
+    "revision": "3cb13c612c078a7cd122",
+    "url": "/static/js/runtime-main.85ce4bb6.js"
+  },
+  {
+    "revision": "d499d8abed29d2ced12170635d1fd620",
+    "url": "/static/media/headerphoto2.d499d8ab.png"
+  },
+  {
+    "revision": "5880919ad0fbf6b44aaa1dc46a608668",
+    "url": "/static/media/iceberg33.5880919a.png"
+  },
+  {
+    "revision": "5fc782bec0cc333bfb7c482f6a441e6b",
+    "url": "/static/media/icebergN1.5fc782be.png"
+  },
+  {
+    "revision": "35234a8847af8227f8ce4726305410dd",
+    "url": "/static/media/icebergN222.35234a88.png"
+  },
+  {
+    "revision": "4d0304e290ca723239858c554d46a70c",
+    "url": "/static/media/instagram-logo.4d0304e2.png"
+  },
+  {
+    "revision": "ae3b5e8baeabd644ff1326f8a2ad8354",
+    "url": "/static/media/logotype.ae3b5e8b.png"
+  },
+  {
+    "revision": "2677e4ece517c5302ca2133489a2b083",
+    "url": "/static/media/micropeel.2677e4ec.jpg"
+  },
+  {
+    "revision": "351132f8661f2df97d7e04e7f76e4b12",
+    "url": "/static/media/num1.351132f8.jpg"
+  },
+  {
+    "revision": "f013e3a344e3751524eb8f089cd5df1f",
+    "url": "/static/media/num2.f013e3a3.jpg"
+  },
+  {
+    "revision": "2accfe5d8b2277b7b0540beabbd1e875",
+    "url": "/static/media/num3.2accfe5d.gif"
+  },
+  {
+    "revision": "0b952dcdad8bbd8c88a65a2173b56423",
+    "url": "/static/media/pinterest.0b952dcd.png"
+  },
+  {
+    "revision": "bc3c9b87311ca3461606ff376ed64dc3",
+    "url": "/static/media/tallpic4.bc3c9b87.gif"
+  },
+  {
+    "revision": "e697fcb8a991b438d52daa17191753bb",
+    "url": "/static/media/twitter.e697fcb8.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cosmoproject/precache-manifest.d2305f58101a3c95b1569b4f6564c830.js"
+  "/precache-manifest.48209cf4959d16c7f7dfc0ed478550dc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cosmoproject/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
